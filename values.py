7c4e5c511aec5ff01225a73b648a5b250624a442
Lukas Jirikovsky <105300002+lukas2932@users.noreply.github.com>
2023-12-08 11:13:29 +0100

    Delete values.py

--- values.py
+++
@@ -1,9 +0,0 @@
-array_of_board = [["|" for _ in range(17)] for _ in range(8)]
-array_of_board_helping = [["|" for _ in range(17)] for _ in range(8)]
-
-array_of_X_pieces = []
-array_of_O_pieces = []
-
-X_queens = [["|" for _ in range(17)] for _ in range(8)]
-O_queens = [["|" for _ in range(17)] for _ in range(8)]
-
